092afd4721c9301f0a083eb21262c18c0d7847f4
Jamezfn <jamesmakachia19@gmail.com>
2025-11-20 10:34:07 +0300

    Implement CRUD operations and user queries in new task files

---
+++ task7.py
@@ -0,0 +1,10 @@
+from app import Session
+from model import User
+from app import Session
+
+session = Session()
+
+users = session.query(User).order_by(User.age.desc()).all()
+
+for user in users:
+    print(f"User {user.id}, name: {user.name}, age: {user.age}")
